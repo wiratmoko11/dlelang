c64300ae99d7d5b019bd9f0676b0ce977eb3ecdf
LendisFabri <lendisfabri27@gmail.com>
2017-03-21 10:35:32 +0700

    Fixed penawaran

--- src/dlelang/controller/UserController.java
+++ src/dlelang/controller/UserController.java
@@ -7,7 +7,15 @@ package dlelang.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import dlelang.implement.TransaksiImplement;
+import dlelang.model.Transaksi;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
 
 /**
  * FXML Controller class
@@ -16,12 +24,41 @@ import javafx.fxml.Initializable;
  */
 public class UserController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private VBox paneMainContainer;
+
+    @FXML
+    private GridPane paneToolBar;
+
+    @FXML
+    private Button menuHome;
+
+    @FXML
+    private Pane paneContentArea;
+
+    @FXML
+    private GridPane paneBottomBar;
+
+    @FXML
+    private AnchorPane paneMenuNavigationDrawer;
+
+    @FXML
+    private AnchorPane paneMenuToolbar;
+
+    @FXML
+    void kirimTransaksi(ActionEvent event) {
+        new TransaksiImplement().insert(new Transaksi(1, 1, "LendisFabri", Integer.parseInt(nilaiTawar.getText())));
+    }
+
+    @FXML
+    private TextField nilaiTawar;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+
+    }
+
 }
